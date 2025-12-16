0ab08c16e027eb737ab84ff401c3af8befe1a498
Ben Chatterton <ben.chatterton@gmail.com>
2025-12-15 20:44:30 -0800

    day 5 complete

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod problem01;
 mod problem02;
 mod problem03;
 mod problem04;
+mod problem05;
 
 fn main() {
     // problem01::main();
     // problem02::find_valid();
     // problem03::find_joltage();
-    problem04::route_forklifts();
+    // problem04::route_forklifts();
+    problem05::check_freshness();
 }
