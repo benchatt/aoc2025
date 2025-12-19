1d45778ab3ec3bf815deef80fbb7c04b0e98d227
Ben Chatterton <bchatterton@mozilla.com>
2025-12-18 17:07:30 -0800

    day 6 part 1

--- src/problem06.rs
+++ src/problem06.rs
@@ -9,6 +9,7 @@ fn parse_math_line(line: String) -> Vec<u8> {
 }
 
 fn mathize(column: Vec<usize>, op: u8) -> usize {
+    // println!("doing {} to {}", op, format!("{:?}", column));
     if op == 0 {
         column.iter().sum()
     } else {
@@ -17,7 +18,7 @@ fn mathize(column: Vec<usize>, op: u8) -> usize {
 }
 
 pub fn squid_math() -> () {
-    let file_to_open = "problem06ex";
+    let file_to_open = "src/data/problem06";
     let file = File::open(file_to_open).unwrap();
     let reader = BufReader::new(file);
     let lines: Vec<String> = reader
@@ -25,10 +26,11 @@ pub fn squid_math() -> () {
         .map(|line| line.unwrap().trim().to_owned())
         .collect();
 
-    let mut table: Vec<Vec<usize>> = vec![vec![]];
+    let mut table: Vec<Vec<usize>> = vec![];
     // let mut table: Grid<usize> = Grid::new(lines.len() - 1, lines[0].split_whitespace().count());
     let mut ops: Vec<u8> = vec![];
     for line in lines {
+        // println!("{}", &line);
         let cs: Vec<char> = line.chars().collect();
         if cs[0] == '*' || cs[0] == '+' {
             ops = parse_math_line(line);
@@ -37,7 +39,16 @@ pub fn squid_math() -> () {
                 .split_whitespace()
                 .map(|token| token.parse::<usize>().unwrap())
                 .collect();
+            // println!("|{}", format!("{:?}", tokenized));
             table.push(tokenized);
         }
     }
+    let res: Vec<usize> = (0..ops.len())
+        .map(|n| {
+            let column = table.iter().map(|row| row[n]).collect();
+            mathize(column, ops[n])
+        })
+        .collect();
+    let sum: usize = res.iter().sum();
+    println!("{}", sum);
 }
