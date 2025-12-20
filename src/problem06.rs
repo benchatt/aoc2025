2c86ed026ac020cf86f1666d12ef548696119f8a
Ben Chatterton <ben.chatterton@gmail.com>
2025-12-20 12:49:51 -0800

    day 06 complete

--- src/problem06.rs
+++ src/problem06.rs
@@ -1,4 +1,3 @@
-use grid::*;
 use std::fs::File;
 use std::io::{BufReader, prelude::*};
 
@@ -17,38 +16,94 @@ fn mathize(column: Vec<usize>, op: u8) -> usize {
     }
 }
 
+fn squidize(cols: Vec<Vec<char>>) -> usize {
+    let mut total: usize = 0;
+    let mut buffer: Vec<usize> = vec![];
+    for mut col in cols {
+        if col.iter().collect::<String>().trim() == "" {
+            continue;
+        }
+        col.pop();
+        let op = col.pop();
+        let stringcol: String = col.into_iter().filter(|c| c != &' ').collect();
+        println!("sc %{}%", stringcol);
+        let num: usize = stringcol.parse().unwrap();
+        match op {
+            Some('*') => {
+                buffer.push(num);
+                for item in &buffer {
+                    print!("{} ", item);
+                }
+                println!("*\n\n");
+                total += buffer.iter().product::<usize>();
+                buffer = vec![];
+            }
+            Some('+') => {
+                buffer.push(num);
+                for item in &buffer {
+                    print!("{} ", item);
+                }
+                println!("+\n\n");
+                total += buffer.iter().sum::<usize>();
+                buffer = vec![];
+            }
+            Some(_) => {
+                buffer.push(num);
+            }
+            None => {
+                panic!("at the disco");
+            }
+        }
+    }
+    total
+}
+
 pub fn squid_math() -> () {
     let file_to_open = "src/data/problem06";
     let file = File::open(file_to_open).unwrap();
     let reader = BufReader::new(file);
     let lines: Vec<String> = reader
         .lines()
-        .map(|line| line.unwrap().trim().to_owned())
+        .map(|line| line.unwrap().to_owned())
         .collect();
 
-    let mut table: Vec<Vec<usize>> = vec![];
-    // let mut table: Grid<usize> = Grid::new(lines.len() - 1, lines[0].split_whitespace().count());
-    let mut ops: Vec<u8> = vec![];
-    for line in lines {
-        // println!("{}", &line);
-        let cs: Vec<char> = line.chars().collect();
-        if cs[0] == '*' || cs[0] == '+' {
-            ops = parse_math_line(line);
-        } else {
-            let tokenized: Vec<usize> = line
-                .split_whitespace()
-                .map(|token| token.parse::<usize>().unwrap())
-                .collect();
-            // println!("|{}", format!("{:?}", tokenized));
-            table.push(tokenized);
-        }
-    }
-    let res: Vec<usize> = (0..ops.len())
+    // let mut table: Vec<Vec<usize>> = vec![];
+    // let mut ops: Vec<u8> = vec![];
+    let charcols: Vec<Vec<char>> = (0..lines[0].len())
         .map(|n| {
-            let column = table.iter().map(|row| row[n]).collect();
-            mathize(column, ops[n])
+            lines
+                .iter()
+                .map(|line| match line.chars().nth(n) {
+                    Some(c) => c,
+                    None => ' ',
+                })
+                .collect()
         })
         .collect();
-    let sum: usize = res.iter().sum();
-    println!("{}", sum);
+
+    // for line in lines {
+    //     let cs: Vec<char> = line.chars().collect();
+    //     if cs[0] == '*' || cs[0] == '+' {
+    //         ops = parse_math_line(line);
+    //     } else {
+    //         let tokenized: Vec<usize> = line
+    //             .split_whitespace()
+    //             .map(|token| token.parse::<usize>().unwrap())
+    //             .collect();
+    //         table.push(tokenized);
+    //     }
+    // }
+    // let res: Vec<usize> = (0..ops.len())
+    //     .map(|n| {
+    //         let column = table.iter().map(|row| row[n]).collect();
+    //         mathize(column, ops[n])
+    //     })
+    //     .collect();
+    // let sum: usize = res.iter().sum();
+    let revcols: Vec<Vec<char>> = charcols.into_iter().rev().collect();
+    for cc in &revcols {
+        let strize: String = cc.iter().collect();
+        println!("@{}@", strize);
+    }
+    println!("{}", squidize(revcols));
 }
